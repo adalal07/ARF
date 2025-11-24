6881c85d5edf4ed251654eff997241f3243aeb36
ARtheboss <30683624+ARtheboss@users.noreply.github.com>
2025-11-24 04:13:28 +0000

    basic skeleton

---
+++ src/tello/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'tello'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ARtheboss',
+    maintainer_email='30683624+ARtheboss@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
